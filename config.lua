8f5c7ab574e122b472ee178aa4f6749b30b2dc9c
ivanse <elivanse@hotmail.com>
2022-06-22 13:33:32 -0300

    22

--- config.lua
+++ config.lua
@@ -83,7 +83,15 @@ lvim.builtin.treesitter.ensure_installed = {
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
-
+vim.api.nvim_create_autocmd("FileType",{
+  pattern = "http",
+  callback = function ()
+    local buff = tonumber(vim.fn.expand("<abuf>"), 10)
+    vim.keymap.set("n", "<leader>rn",rest_nvim.run, { noremap = true, buffer = buff})
+    vim.keymap.set("n", "<leader>rl",rest_nvim.last, { noremap = true, buffer = buff})
+    vim.keymap.set("n", "<leader>rp", function() rest_nvim.run(true) end, { noremap = true, buffer = buff})
+  end
+})
 -- generic LSP settings
 
 -- ---@usage disable automatic installation of servers
@@ -154,7 +162,37 @@ lvim.plugins = {
   { "vim-scripts/vim-gitgutter" },
   { "vim-jp/vim-go-extra" },
   { "nsf/gocode" },
-  {"NTBBloodbath/rest.nvim"}
+  { "nvim-lua/plenary.nvim"},
+  {
+  "NTBBloodbath/rest.nvim",
+  requires = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("rest-nvim").setup({
+      -- Open request results in a horizontal split
+      result_split_horizontal = false,
+      -- Keep the http file buffer above|left when split horizontal|vertical
+      result_split_in_place = false,
+      -- Skip SSL verification, useful for unknown certificates
+      skip_ssl_verification = false,
+      -- Highlight request on run
+      highlight = {
+        enabled = true,
+        timeout = 150,
+      },
+      result = {
+        -- toggle showing URL, HTTP info, headers at top the of result window
+        show_url = true,
+        show_http_info = true,
+        show_headers = true,
+      },
+      -- Jump to request line on run
+      jump_to_request = false,
+      env_file = '.env',
+      custom_dynamic_variables = {},
+      yank_dry_run = true,
+    })
+  end
+},
 } 
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
