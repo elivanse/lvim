5fe253f7d0338f7c1ceebb8ea3b8a6dc356b7944
IvanMaximilianoPereyra <elivanse@hotmail.com>
2022-06-28 21:52:11 -0300

    2

--- config.lua
+++ config.lua
@@ -20,6 +20,8 @@ vim.g.ayucolor = 'mirage'
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<C-a>"] = ":terminal<cr>"
+lvim.keys.normal_mode["<C-h>"] = ":Http<cr>"
 -- unmap a default keymapping
 -- vim.keymap.del("n", "<C-Up>")
 -- override a default keymapping
@@ -148,13 +150,36 @@ lvim.builtin.treesitter.highlight.enabled = true
 lvim.plugins = {
   { "mhartington/oceanic-next" },
   { "ayu-theme/ayu-vim" },
-  { "luisiacc/gruvbox-baby"},
+  { "luisiacc/gruvbox-baby" },
   { "folke/lsp-colors.nvim" },
   { "vim-scripts/vim-gitgutter" },
   { "vim-jp/vim-go-extra" },
   { "nsf/gocode" },
-  {"nicwest/vim-http"},
-} 
+  { "nicwest/vim-http" },
+  { "kien/rainbow_parentheses.vim" },
+}
+let g:rbpt_colorpairs = [
+    \ ['brown',       'RoyalBlue3'],
+    \ ['Darkblue',    'SeaGreen3'],
+    \ ['darkgray',    'DarkOrchid3'],
+    \ ['darkgreen',   'firebrick3'],
+    \ ['darkcyan',    'RoyalBlue3'],
+    \ ['darkred',     'SeaGreen3'],
+    \ ['darkmagenta', 'DarkOrchid3'],
+    \ ['brown',       'firebrick3'],
+    \ ['gray',        'RoyalBlue3'],
+    \ ['black',       'SeaGreen3'],
+    \ ['darkmagenta', 'DarkOrchid3'],
+    \ ['Darkblue',    'firebrick3'],
+    \ ['darkgreen',   'RoyalBlue3'],
+    \ ['darkcyan',    'SeaGreen3'],
+    \ ['darkred',     'DarkOrchid3'],
+    \ ['red',         'firebrick3'],
+    \ ]
+
+let g:rbpt_max = 16
+
+let g:rbpt_loadcmd_toggle = 0
 
 
 
